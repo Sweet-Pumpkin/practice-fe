ab7cd016abffbc19dd9fd3fc7560b0c411280d5c
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-05 15:52:13 +0100

    feat: Image slider js - add next btn & prev btn

--- part1/slider-js/src/js/imageSlider.js
+++ part1/slider-js/src/js/imageSlider.js
@@ -1,8 +1,15 @@
 class ImageSlider {
     /** 선언 */
+    /** private field - class 외부에서 값을 조회하거나 값을 덮어 씌울 수 없음 */
     #currentPosition = 0;
     #sliderNumber = 0;
     #sliderWidth = 0;
+
+    /** public field */
+    sliderWrapEl;
+    sliderListEl;
+    nextBtnEl;
+    previousBtnEl;
 
     /** 실행 */
     constructor() {
@@ -11,6 +18,19 @@ class ImageSlider {
 
     /** 탐색 */
     assignElement() {
+        this.sliderWrapEl = document.getElementById('slider-wrap');
+        this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+        this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+        this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    }
 
+    /** 슬라이더 갯수 계산 */
+    initSliderNumber() {
+        this.#sliderNumber = this.sliderListEl.querySelectorAll('li').length;
+    }
+
+    /** 슬라이더 가로 길이 계산 */
+    initSliderWidth() {
+        this.#sliderWidth = this.sliderListEl.clientWidth;
     }
 }
