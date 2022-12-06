478f9168dea226c909104890cbfaa4df56755f3d
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-06 16:00:28 +0100

    feat: Image slider js - add next & prev btn

--- part1/slider-js/src/js/imageSlider.js
+++ part1/slider-js/src/js/imageSlider.js
@@ -1,4 +1,6 @@
-class ImageSlider {
+// export => 함수명을 그대로 사용해야 함.
+// export default => 함수명을 원하는 대로 사용 가능.
+export default class ImageSlider {
     /** 선언 */
     /** private field - class 외부에서 값을 조회하거나 값을 덮어 씌울 수 없음 */
     #currentPosition = 0;
@@ -13,7 +15,11 @@ class ImageSlider {
 
     /** 실행 */
     constructor() {
-
+        this.assignElement();
+        this.initSliderNumber();
+        this.initSliderWidth();
+        this.initSliderListWidth();
+        this.addEvent();
     }
 
     /** 탐색 */
@@ -33,4 +39,40 @@ class ImageSlider {
     initSliderWidth() {
         this.#sliderWidth = this.sliderListEl.clientWidth;
     }
+
+    /** 슬라이더 리스트 가로 길이 동적 할당 */
+    initSliderListWidth() {
+        this.sliderListEl.style.width = `${this.#sliderNumber * this.#sliderWidth}px`;
+    }
+
+    /** 이벤트 실행 */
+    addEvent() {
+        this.nextBtnEl.addEventListener('click', this.moveToRigth.bind(this));
+        this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    }
+
+    /** 슬라이드를 오른쪽으로 움직이게 하는 함수 */
+    moveToRigth() {
+        this.#currentPosition++;
+
+        /** 경계값 설정 */
+        if (this.#currentPosition === this.#sliderNumber) {
+            this.#currentPosition = 0;
+        }
+
+        this.sliderListEl.style.left = `-${this.#sliderWidth * this.#currentPosition}px`;
+    }
+
+    /** 슬라이드를 왼쪽으로 움직이게 하는 함수 */
+    moveToLeft() {
+        this.#currentPosition--;
+
+        /** 경계값 설정 */
+        if (this.#currentPosition === -1) {
+            this.#currentPosition = this.#sliderNumber - 1;
+        }
+
+        this.sliderListEl.style.left = `-${this.#sliderWidth * this.#currentPosition}px`;
+    }
+
 }
