6f2db90f14f7afcccfb9a821c909a7b3ad87e3fc
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-12-15 19:49:50 +0100

    feat: Datepicker js - Add searching date option

--- part1/datepicker-js/src/js/index.js
+++ part1/datepicker-js/src/js/index.js
@@ -1,1 +1,49 @@
-console.log("hello??");
+class DatePicker {
+  /** 선언 */
+  monthData = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  #calendarDate = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  #selected = {
+    data: '',
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  /** 실행 */
+  constructor() {}
+
+  /** 캘린더 날짜 초기화 */
+  initCalendarDate() {
+    const data = new Date();
+    const date = data.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+}
